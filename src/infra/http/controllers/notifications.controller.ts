9f527db0e4b0ad14297407062383d1e532985713
lvieira268 <lvieira268@gmail.com>
2022-12-14 19:11:37 -0300

    feat: create notifications controller
    Signed-off-by: lvieira268 <lvieira268@gmail.com>

---
+++ src/infra/http/controllers/notifications.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { SendNotification } from '../../../app/use-cases/send-notification';
+
+import { CreateNoticationBody } from '../dtos/create-notifications-body';
+
+@Controller('notifications')
+export class NotificationsController {
+	constructor(private sendNotification: SendNotification) {}
+	@Post()
+	async create(@Body() body: CreateNoticationBody) {
+		const { content, category, recipientId } = body;
+		const { notification } = await this.sendNotification.execute({
+			recipientId,
+			content,
+			category,
+		});
+
+		return { notification };
+	}
+}
