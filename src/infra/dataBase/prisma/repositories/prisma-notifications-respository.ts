8fd528c7fcf4a9b19dea2f7ab03ca119014fa943
lvieira268 <lvieira268@gmail.com>
2022-12-14 19:14:53 -0300

    feat: create database repository of notifications
    Signed-off-by: lvieira268 <lvieira268@gmail.com>

---
+++ src/infra/dataBase/prisma/repositories/prisma-notifications-respository.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+
+import { Notification } from '../../../../app/entities/notification';
+import { NotificationsRepository } from '../../../../app/repositories/notifications-repository';
+
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class PrismaNotificationRepository implements NotificationsRepository {
+	constructor(private prismaService: PrismaService) {}
+
+	async create(notification: Notification): Promise<void> {
+		await this.prismaService.notification.create({
+			data: {
+				id: notification.id,
+				recipientId: notification.recipientId,
+				content: notification.content.value,
+				category: notification.category,
+				createdAt: notification.createdAd,
+				readAt: notification.readAt,
+			},
+		});
+	}
+}
