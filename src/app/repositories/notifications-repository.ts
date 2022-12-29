d0d7529a8a327c79f0d068feded49d9669727ceb
lvieira268 <lvieira268@gmail.com>
2022-12-29 16:54:11 -0300

    feat: create repositories to find, save and count
    Signed-off-by: lvieira268 <lvieira268@gmail.com>

--- src/app/repositories/notifications-repository.ts
+++ src/app/repositories/notifications-repository.ts
@@ -2,4 +2,10 @@ import { Notification } from '../entities/notification';
 
 export abstract class NotificationsRepository {
 	abstract create(notification: Notification): Promise<void>;
+	abstract findById(notificationId: string): Promise<Notification | null>;
+	abstract save(notification: Notification): Promise<void>;
+	abstract countManyByRecipientById(recipientId: string): Promise<number>;
+	abstract findManyByRecipientById(
+		recipientId: string,
+	): Promise<Notification[]>;
 }
