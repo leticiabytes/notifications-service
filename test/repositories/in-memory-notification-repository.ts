55ebefbd2f70a610213610e2e877a91b2610af7e
lvieira268 <lvieira268@gmail.com>
2022-12-30 18:47:55 -0300

    test: in-memory-notification-repository
    Signed-off-by: lvieira268 <lvieira268@gmail.com>

--- test/repositories/in-memory-notification-repository.ts
+++ test/repositories/in-memory-notification-repository.ts
@@ -4,7 +4,41 @@ import { Notification } from '@app/entities/notification';
 export class InMemoryNoticationsRepository implements NotificationsRepository {
 	public notifications: Notification[] = [];
 
+	async findById(notificationId: string): Promise<Notification | null> {
+		const notification = this.notifications.find(
+			(item) => item.id === notificationId,
+		);
+
+		if (!notification) {
+			return null;
+		}
+
+		return notification;
+	}
+
+	async countManyByRecipientById(recipientId: string): Promise<number> {
+		return this.notifications.filter(
+			(notification) => notification.recipientId === recipientId,
+		).length;
+	}
+
+	async findManyByRecipientById(recipientId: string): Promise<Notification[]> {
+		return this.notifications.filter(
+			(notification) => notification.recipientId === recipientId,
+		);
+	}
+
 	async create(notification: Notification) {
 		this.notifications.push(notification);
+	}
+
+	async save(notification: Notification): Promise<void> {
+		const notificationIndex = this.notifications.findIndex(
+			(item) => item.id === notification.id,
+		);
+
+		if (notificationIndex >= 0) {
+			this.notifications[notificationIndex] = notification;
+		}
 	}
 }
